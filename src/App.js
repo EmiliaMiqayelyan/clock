2bedb17cc400e0ccd7dc4cb3d2d88d6d8af1522e
emiliamiqaelyan <emilik200915@gmail.com>
2023-01-30 23:07:37 +0400

    final clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Clock from "./clock/Clock";
-import Alarm from "./clock/Alarm"
 
 function App() {
   return (
     <div className="App">
       <Clock />
-      <Alarm/>
     </div>
   );
 }
